a65d0e8a7364598b0eb33db930f72f49efb2e257
mertyalcinnn <mert.yalcinn5@gmail.com>
2025-03-07 02:27:43 +0300

    test3

---
+++ src/components/preorder/OrderForm.tsx
@@ -0,0 +1,8 @@
+export default function OrderForm() {
+  return (
+    <div className="mb-8">
+      <h2 className="text-2xl font-semibold mb-4">Order Details</h2>
+      <form>{/* Form alanları */}</form>
+    </div>
+  );
+}
